106b7c1250d9f4e3f681b43c70380ced1aa97d7c
saleh2411 <salehkhalil1124@gmail.com>
2026-05-27 22:14:32 +0300

    new order

--- LAB_04/Stopwatch.v
+++ LAB_04/Stopwatch.v
@@ -39,6 +39,83 @@ module Stopwatch(clk, btnC, btnU, btnR, btnL, seg, an, dp, led_left, led_right);
     wire trig_left, split_left, init_regs_left, count_enabled_left;
     reg selected_stopwatch;
     
-	// FILL HERE INSTANTIATIONS 
+	// FILL HERE INSTANTIATIONS
+    wire reset, trig, split, toggle = 1'b1;
+
+    ///////////////////////////
+	// Buttons Stabilization //
+    ///////////////////////////
+	Debouncer #(.COUNTER_BITS(20)) debounce_reset(
+	.clk(clk),
+	.input_unstable(btnC),
+	.output_stable(reset)
+	);
+	
+	Debouncer #(.COUNTER_BITS(20)) debounce_trigger(
+	.clk(clk),
+	.input_unstable(btnU),
+	.output_stable(trig)
+	);
+	
+	Debouncer #(.COUNTER_BITS(20)) debounce_split(
+	.clk(clk),
+	.input_unstable(btnR),
+	.output_stable(split)
+	);
+	
+	Debouncer #(.COUNTER_BITS(20)) debounce_toggle(
+	.clk(clk),
+	.input_unstable(btnL),
+	.output_stable(toggle)
+	);
+
+    ///////////////////////////
+    //        Controls       //
+    ///////////////////////////
+    Ctl ctl_right(
+        .clk(clk),
+        .reset(reset),
+        .trig(trig_right),
+        .split(split_right),
+        .init_regs(init_regs_right),
+        .count_enabled(count_enabled_right)
+    );
+    Ctl ctl_left(
+        .clk(clk),
+        .reset(reset),
+        .trig(trig_left),
+        .split(split_left),
+        .init_regs(init_regs_left),
+        .count_enabled(count_enabled_left)
+    );
+
+    ///////////////////////////
+    //   7-Segment Display   //
+    ///////////////////////////
+    Seg_7_Display display(
+        .sw(time_reading),
+        .clk(clk),
+        .dp(0),
+        .seg(seg),
+        .an(an),
+        .dp(dp)
+    );
+
+    ///////////////////////////
+    //        Counters       //
+    ///////////////////////////
+    
+
+
+
+    ///////////////////////////
+    ///////////////////////////
+    ///////////////////////////
+    ///////////////////////////
+    ///////////////////////////
+    ///////////////////////////
+    ///////////////////////////
+    ///////////////////////////
+
 
 endmodule
