9513dfdc0d86c7c204f19ab12695e1fed50f00d2
saleh2411 <salehkhalil1124@gmail.com>
2026-06-23 21:54:27 +0300

    Game State Machine

---
+++ LAB_07/GSM.v
@@ -0,0 +1,27 @@
+`timescale 1ns/10ps
+//////////////////////////////////////////////////////////////////////////////////
+//    Game State Machine
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module GSM(
+    input clk,
+    input reset,
+    input crash,
+    );
+    localparam IDLE = 2'b00;
+    localparam PLAY = 2'b01;
+    localparam GAME_OVER = 2'b10;
+
+    reg [1:0] state = IDLE;
+    
+
+    always @(posedge clk) begin
+        if(reset) begin
+            state <= IDLE;
+        end else begin
+
+        end
+    end
+
+endmodule
